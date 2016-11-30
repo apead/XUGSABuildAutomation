07d48422757c518314c6dbdf65f13f3cd8e16e7c
Allan Pead <Allan Pead>
2016-11-30 14:08:33 +0200

    Added quick Test Cloud demo during presentation.

---
+++ AzureEasyTables/CrossPlatformDemo/CrossPlatformDemo.Tests/AppInitializer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace CrossPlatformDemo.Tests
+{
+    public class AppInitializer
+    {
+  /*      public static IApp StartApp(Platform platform)
+        {
+            if (platform == Platform.Android)
+            {
+                return ConfigureApp.Android.ApkFile("C:\\work\\samples\\XUGSABuildAutomation\\AzureEasyTables\\CrossPlatformDemo\\Releases\\za.co.ctxug.CrossPlatformDemo.apk").StartApp();
+            }
+
+            return ConfigureApp
+                .iOS
+                .StartApp();
+        }
+        */
+    }
+}
+
---
+++ AzureEasyTables/CrossPlatformDemo/CrossPlatformDemo.Tests/Tests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace CrossPlatformDemo.Tests
+{
+    [TestFixture(Platform.Android)]
+    [TestFixture(Platform.iOS)]
+    public class Tests
+    {
+        IApp app;
+        Platform platform;
+
+        public Tests(Platform platform)
+        {
+            this.platform = platform;
+        }
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            app = ConfigureApp.Android.ApkFile("C:\\work\\samples\\XUGSABuildAutomation\\AzureEasyTables\\CrossPlatformDemo\\Releases\\za.co.ctxug.CrossPlatformDemo.apk").StartApp();
+        }
+
+        [Test]
+        public void AppLaunches()
+        {
+            app.Screenshot("First screen.");
+            app.WaitForElement(c => c.Marked("iOS"),"Time Out",new TimeSpan(0,0,5,0));
+            app.Screenshot("Latte Panda Master.");
+            app.Tap(c => c.Marked("Latte Panda"));
+            app.Screenshot("Latte Panda Detail.");
+        }
+
+  //      [Test]
+  //      public void ReplIt()
+  //      {
+  //          app.Repl();
+  //      }
+
+    }
+}
+
